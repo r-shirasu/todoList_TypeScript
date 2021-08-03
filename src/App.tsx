3ad1be61a07023982486acea78c294768fe0733d
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-08-03 18:52:02 +0900

    見た目を作る

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.scss";
 
-export const App = () => {
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>TO-DO LIST</h1>
+      <form id="add">
+        <input type="text" placeholder="new task" />
+        <input type="submit" value="ADD" />
+      </form>
+      <div className="tasksBoard">
+        <ul id="todo-list">
+          {todos.map((todo, index) => (
+            <li key={`${todo}${index}`}>
+              <span>×</span>
+              <label>{todo}</label>
+            </li>
+          ))}
+        </ul>
+        <p>Clear</p>
+      </div>
     </div>
   );
 };
