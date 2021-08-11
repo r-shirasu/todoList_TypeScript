d3f1c530ec4f166bd274e6df6c345d02b245fb05
shirasu-aq <82349387+shirasu-aq@users.noreply.github.com>
2021-08-11 09:41:20 +0900

    TypeScriptでtodoリストを実装する (#9)
    * 見た目を作る
    
    * 入力フォームの実装
    
    * todoを登録しリストに表示させる
    
    * 空文字だったときにアラートメッセージを表示&リストに追加されるとフォームを空にする
    
    * クリアボタン実装
    
    * 削除機能の実装
    
    * チェック機能のイベントを定義する
    
    * labelにクラスを付与してチェックされたら要素が変わるようにする
    
    * App.testにもnamed exportしておく
    
    * interfaceではなくtypeを使う
    
    * anyは使わない。unknownを用いる。
    
    * indexにはnumber型を使用する

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,100 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.scss";
 
-export const App = () => {
+type Todo = {
+  description: string;
+  isChecked: boolean;
+};
+
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [task, setTask] = useState("");
+  const [isShowAlertMessage, setIsShowMessage] = useState(false);
+
+  const onChangeForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
+  const newToDo: Todo = {
+    description: task,
+    isChecked: false,
+  };
+
+  // todo追加機能
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (task === "") {
+      setIsShowMessage(true);
+      return;
+    }
+
+    setIsShowMessage(false);
+    setTodos([newToDo, ...todos]);
+    setTask("");
+  };
+
+  // クリア機能
+  const clearAllTodos = () => {
+    setTodos([]);
+  };
+
+  // 特定のtodoを削除する機能
+  const handleOnDelete = (index: number) => {
+    const deleteArr = todos.filter((_, id) => {
+      return id !== index;
+    });
+    setTodos(deleteArr);
+  };
+
+  // チェック機能
+  const handleOnCheck = (index: number) => {
+    const checkedTodos = todos.map((todo, _index) => {
+      if (_index !== index) {
+        return todo;
+      }
+      return {
+        description: todo.description,
+        isChecked: !todo.isChecked,
+      };
+    });
+    setTodos(checkedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>TO-DO LIST</h1>
+      <form onSubmit={handleSubmit} id="add">
+        <input
+          type="text"
+          placeholder="new task"
+          value={task}
+          onChange={(e) => onChangeForm(e)}
+        />
+        <input type="submit" value="ADD" />
+      </form>
+      {isShowAlertMessage && (
+        <div className="alertMessage">Todoを入力してください</div>
+      )}
+      <div className="tasksBoard">
+        <ul id="todo-list">
+          {todos.map((todo, index) => (
+            <li key={`${todo}${index}`}>
+              <span onClick={() => handleOnDelete(index)}>×</span>
+              <label className={todo.isChecked ? "checked" : ""}>
+                <input
+                  type="checkbox"
+                  checked={todo.isChecked}
+                  name="check"
+                  onChange={() => handleOnCheck(index)}
+                />
+                {todo.description}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <p onClick={clearAllTodos}>Clear</p>
+      </div>
     </div>
   );
 };
